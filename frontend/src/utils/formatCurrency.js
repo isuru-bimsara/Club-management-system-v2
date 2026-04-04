cff3d2cac961603ac01f0c790e119a1ae57edda1
isuru-bimsara <160391646+isuru-bimsara@users.noreply.github.com>
2026-04-04 16:30:24 +0530

    Delete frontend directory

--- frontend/src/utils/formatCurrency.js
+++
@@ -1,8 +0,0 @@
-export const formatCurrency = (amount) => {
-  return new Intl.NumberFormat('en-LK', {
-    style: 'currency',
-    currency: 'LKR',
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-  }).format(amount);
-};
